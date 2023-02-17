3155c3ea8397debfbf2fa886d803eaa2c83a11b8
Diego Alejandro Rubi <00121117@uca.edu.sv>
2023-02-17 11:24:57 -0600

    Creando iconos y agregando libreria a comentarios

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Images.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Images.kt
@@ -3,10 +3,14 @@ package com.example.jetpackcomposecatalogo
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +24,6 @@ fun MyImage() {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
 fun MyImageAdvance() {
     Image(
@@ -31,4 +34,16 @@ fun MyImageAdvance() {
             .clip(CircleShape)
             .border(5.dp, Color.Red, CircleShape)
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyIcon() {
+    // es parte de material design y es de 24x24
+    Icon(
+        imageVector = Icons.Rounded.Star,
+        contentDescription = "Mi icono",
+        tint= Color.Blue
+    )
+    // Material Icon Extended : Para tener mas iconos
 }
