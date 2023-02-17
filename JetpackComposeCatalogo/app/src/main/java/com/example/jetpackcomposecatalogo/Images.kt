1d1fea2461188415b8caefdf1fb632defcea226c
Diego Alejandro Rubi <00121117@uca.edu.sv>
2023-02-17 11:15:01 -0600

    Imagen avanzada

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Images.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/Images.kt
@@ -1,16 +1,34 @@
 package com.example.jetpackcomposecatalogo
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-@Preview(showBackground = true)
 @Composable
 fun MyImage() {
     Image(
         painter = painterResource(id = R.drawable.ic_launcher_background),
         contentDescription = "Ejemplo",
         alpha = 0.5f
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyImageAdvance() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Ejemplo",
+        // modifier = Modifier.clip(RoundedCornerShape(25f))
+        modifier = Modifier
+            .clip(CircleShape)
+            .border(5.dp, Color.Red, CircleShape)
     )
 }
