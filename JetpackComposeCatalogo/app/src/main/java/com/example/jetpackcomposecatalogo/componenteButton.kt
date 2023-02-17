bb45912712ba3ac0996af65435711791c2b8e6f2
Diego Alejandro Rubi <00121117@uca.edu.sv>
2023-02-17 10:07:57 -0600

    Crear boton con estado enabled guardado en una variable

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/componenteButton.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/componenteButton.kt
@@ -9,6 +9,11 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,13 +22,17 @@ import androidx.compose.ui.unit.dp
 @Preview(showBackground = true)
 @Composable
 fun MyButtonExample() {
+    var enabled by rememberSaveable {
+        mutableStateOf(true)
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(24.dp)
     ) {
         Button(
-            onClick = { Log.i("Test", "Ejemplo") },
+            onClick = { enabled= false },
+            enabled = enabled,
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Color.Magenta,
                 contentColor = Color.Blue
@@ -31,8 +40,6 @@ fun MyButtonExample() {
             border = BorderStroke(5.dp,Color.Green)
         ) {
             Column() {
-                Text(text = "Hola")
-                Text(text = "Hola")
                 Text(text = "Hola")
             }
         }
